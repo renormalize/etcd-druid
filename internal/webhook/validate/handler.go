6574188b1af1d0518b08766f6fb5de2ee909123c
Saketh Kalaga <51327242+renormalize@users.noreply.github.com>
2024-05-29 12:31:16 +0530

    Etcd validation code in `api/validation` is now run for the validating webhook
    * Run the code in `api/validation` in the validating webhook
    
    * Prefix changed in `config/samples/druid_v1alpha1_etcd_localstack.yaml`
      store to include namespace and name since this is required.
    
    * Prefix changed in `config/samples/druid_v1alpha1_etcd.yaml` store
      to include namespace and name since this is required.

--- internal/webhook/validate/handler.go
+++ internal/webhook/validate/handler.go
@@ -8,7 +8,11 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/gardener/etcd-druid/api/v1alpha1"
+	druidvalidation "github.com/gardener/etcd-druid/api/validation"
 	"github.com/go-logr/logr"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
@@ -22,7 +26,7 @@ type Handler struct {
 	logger  logr.Logger
 }
 
-// NewHandler creates a new handler for Sentinel Webhook.
+// NewHandler creates a new handler for Validating Webhook.
 func NewHandler(mgr manager.Manager, config *Config) (*Handler, error) {
 	decoder := admission.NewDecoder(mgr.GetScheme())
 	return &Handler{
@@ -38,4 +42,27 @@ func (h *Handler) Handle(ctx context.Context, req admission.Request) admission.R
 	fmt.Println("The validating webhook was called")
 	h.logger.Info("The validating webhook was called")
 	return admission.Allowed(fmt.Sprintf("valid etcd-druid"))
+}
+
+func (h *Handler) ValidateCreate(_ context.Context, obj runtime.Object) (admission.Warnings, error) {
+	object := obj.(*v1alpha1.Etcd)
+	if errs := druidvalidation.ValidateEtcd(object); len(errs) > 0 {
+		return nil, apierrors.NewInvalid(object.GroupVersionKind().GroupKind(), object.GetName(), errs)
+	}
+	h.logger.Info("The validating webhook was called for create operation")
+	return nil, nil
+}
+
+func (h *Handler) ValidateUpdate(_ context.Context, oldObj, newObj runtime.Object) (admission.Warnings, error) {
+	object := newObj.(*v1alpha1.Etcd)
+	if errs := druidvalidation.ValidateEtcdUpdate(object, oldObj.(*v1alpha1.Etcd)); len(errs) > 0 {
+		return nil, apierrors.NewInvalid(object.GroupVersionKind().GroupKind(), object.GetName(), errs)
+	}
+	h.logger.Info("The validating webhook was called for update operation")
+	return nil, nil
+}
+
+func (h *Handler) ValidateDelete(_ context.Context, _ runtime.Object) (admission.Warnings, error) {
+	h.logger.Info("The validating webhook was called for delete operation")
+	return nil, nil
 }
--- internal/webhook/validate/register.go
+++ internal/webhook/validate/register.go
@@ -5,23 +5,29 @@
 package validate
 
 import (
+	"github.com/gardener/etcd-druid/api/v1alpha1"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 )
 
 const (
 	// handlerName is the name of the webhook handler.
-	handlerName = "validate-webhook"
+	handlerName = "validating-webhook"
 	// WebhookPath is the path at which the handler should be registered.
 	webhookPath = "/webhooks/validate"
 )
 
 // RegisterWithManager registers Handler to the given manager.
 func (h *Handler) RegisterWithManager(mgr manager.Manager) error {
-	webhook := &admission.Webhook{
-		Handler:      h,
-		RecoverPanic: true,
-	}
+	// webhook := &admission.Webhook{
+	// 	Handler:      h,
+	// 	RecoverPanic: true,
+	// }
+	webhook := admission.WithCustomValidator(
+		mgr.GetScheme(),
+		&v1alpha1.Etcd{},
+		h).
+		WithRecoverPanic(true)
 
 	mgr.GetWebhookServer().Register(webhookPath, webhook)
 	return nil
